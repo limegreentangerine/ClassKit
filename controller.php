f615d8e1591bd44f49f5caafb0bbcca6b60d00bc
Lee Jones <prolific.jones82@gmail.com>
2026-09-17 14:03:28 +0100

    v1.0

--- controller.php
+++ controller.php
@@ -22,7 +22,7 @@ class Controller extends Package
      *
      * @var string
      */
-    protected $pkgVersion = '0.0.0';
+    protected $pkgVersion = '1.0.0';
 
     /**
      * The minimum Concrete version compatible with the package.
